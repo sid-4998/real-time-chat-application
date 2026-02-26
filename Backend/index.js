07c0209a041fa3014313c67c195f597e5b572ca2
sid-4998 <siddhantjha34@gmail.com>
2026-02-26 20:41:22 +0530

    Configured the database connection and fixed dns server and Ip white-listing error

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
 
 dotenv.config();
 
 const PORT = process.env.PORT
 const app = express();
 
-
-
+// Connect to database
+connectDatabase();
 
 
 
